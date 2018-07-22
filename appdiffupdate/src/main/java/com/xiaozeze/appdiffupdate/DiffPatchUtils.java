fd8ae2bb8ac5ae97fc6376698a94a78976c7863d
fengzeyuan <hanchanpo@163.com>
2018-07-22 19:33:18 +0800

    增量更新

--- appdiffupdate/src/main/java/com/xiaozeze/appdiffupdate/DiffPatchUtils.java
+++ appdiffupdate/src/main/java/com/xiaozeze/appdiffupdate/DiffPatchUtils.java
@@ -7,21 +7,36 @@ package com.xiaozeze.appdiffupdate;
  * Version: 1.0
  */
 public class DiffPatchUtils {
-	
-	static DiffPatchUtils instance;
 
-	public static DiffPatchUtils getInstance() {
-		if (instance == null)
-			instance = new DiffPatchUtils();
-		return instance;
-	}
+    static DiffPatchUtils instance;
 
-	static {
-		System.loadLibrary("xz_diff_patch");
-	}
+    public static DiffPatchUtils getInstance() {
+        if (instance == null)
+            instance = new DiffPatchUtils();
+        return instance;
+    }
 
-	public native int genDiff(String oldApkPath, String newApkPath, String patchPath);
+    static {
+        System.loadLibrary("xz_diff_patch");
+    }
 
-	public native int patch(String oldApkPath, String newApkPath, String patchPath);
+    /**
+     * 获取差分文件
+     *
+     * @param oldApkPath 老文件地址
+     * @param newApkPath 新文件地址
+     * @param patchPath  差分包生成地址
+     * @return 正常返回0
+     */
+    public native int genDiff(String oldApkPath, String newApkPath, String patchPath);
+
+    /**
+     * 合并差量包
+     * @param oldApkPath  老文件地址
+     * @param newApkPath  新文件地址
+     * @param patchPath   补丁地址
+     * @return 正常返回0
+     */
+    public native int patch(String oldApkPath, String newApkPath, String patchPath);
 
 }
