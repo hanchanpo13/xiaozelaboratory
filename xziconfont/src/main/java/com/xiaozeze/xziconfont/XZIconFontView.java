ccd01afc7063cd538ff814186dcd7a17aa3091e0
fengzeyuan <fengzeyuan@xiaozhu.com>
2018-09-20 17:58:32 +0800

    iconFont库V2.0

--- xziconfont/src/main/java/com/xiaozeze/xziconfont/XZIconFontView.java
+++ xziconfont/src/main/java/com/xiaozeze/xziconfont/XZIconFontView.java
@@ -6,12 +6,10 @@ import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.support.annotation.ColorInt;
-import android.support.annotation.IntRange;
 import android.support.annotation.StringRes;
 import android.text.TextPaint;
 import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.util.SparseArray;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.RelativeLayout;
@@ -43,8 +41,10 @@ public class XZIconFontView extends RelativeLayout {
     public static final int BOTTOM = RIGHT + 1;
 
     private TextView mIconView;
+    private TextBound mIconBound = new TextBound();
 
-    private SparseArray<TextBound> mBoundsMap = new SparseArray<>(5);
+    private TextView mTextView;
+    private TextBound mTextBound = new TextBound();
 
     public XZIconFontView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
@@ -56,94 +56,103 @@ public class XZIconFontView extends RelativeLayout {
         View.inflate(getContext(), R.layout.icon_font, this);
         mIconView = findViewById(R.id.fontViewContent);
         // 解析XML属性
-        TextBound iconBound = new TextBound();
-        int leftPadding = 0;
-        int topPadding = 0;
-        int rightPadding = 0;
-        int bottomPadding = 0;
 
         // 获取自定义属性
         TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.XZIconFontView);
         // 图标相关
-        iconBound.mText = ta.getText(R.styleable.XZIconFontView_iconCode);
-        iconBound.mColor = ta.getColor(R.styleable.XZIconFontView_iconColor, Color.BLACK);
-        iconBound.mSize = ta.getDimension(R.styleable.XZIconFontView_iconSize, dp2px(15));
-        iconBound.mStyle = ta.getInt(R.styleable.XZIconFontView_iconBold, Typeface.NORMAL);
-        leftPadding = (int) ta.getDimension(R.styleable.XZIconFontView_textLeftPadding, 0);
-        topPadding = (int) ta.getDimension(R.styleable.XZIconFontView_textTopPadding, 0);
-        rightPadding = (int) ta.getDimension(R.styleable.XZIconFontView_textRightPadding, 0);
-        bottomPadding = (int) ta.getDimension(R.styleable.XZIconFontView_textBottomPadding, 0);
+        mIconBound.orientation = ta.getInt(R.styleable.XZIconFontView_icon_rotation, LEFT);
+        mIconBound.mText = ta.getText(R.styleable.XZIconFontView_iconCode);
+        mIconBound.mColor = ta.getColor(R.styleable.XZIconFontView_iconColor, Color.BLACK);
+        mIconBound.mSize = ta.getDimension(R.styleable.XZIconFontView_iconSize, dp2px(15));
+        mIconBound.mStyle = ta.getInt(R.styleable.XZIconFontView_iconBold, Typeface.NORMAL);
 
         // 文字相关
-
-        //  左
-        TextBound textBound = new TextBound();
-        textBound.mText = ta.getText(R.styleable.XZIconFontView_font_leftText);
-        textBound.mColor = ta.getColor(R.styleable.XZIconFontView_font_leftTextColor, iconBound.mColor);
-        textBound.mSize = ta.getDimension(R.styleable.XZIconFontView_font_leftTextSize, iconBound.mSize + dp2px(2));
-        textBound.mStyle = ta.getInt(R.styleable.XZIconFontView_font_leftTextStyle, Typeface.NORMAL);
-        mBoundsMap.put(LEFT, textBound);
-
-        // 上
-        textBound = new TextBound();
-        textBound.mText = ta.getText(R.styleable.XZIconFontView_font_topText);
-        textBound.mColor = ta.getColor(R.styleable.XZIconFontView_font_topTextColor, iconBound.mColor);
-        textBound.mSize = ta.getDimension(R.styleable.XZIconFontView_font_topTextSize, iconBound.mSize + dp2px(2));
-        textBound.mStyle = ta.getInt(R.styleable.XZIconFontView_font_topTextStyle, Typeface.NORMAL);
-        mBoundsMap.put(TOP, textBound);
-
-        // 右
-        textBound = new TextBound();
-        textBound.mText = ta.getText(R.styleable.XZIconFontView_font_rightText);
-        textBound.mColor = ta.getColor(R.styleable.XZIconFontView_font_rightTextColor, iconBound.mColor);
-        textBound.mSize = ta.getDimension(R.styleable.XZIconFontView_font_rightTextSize, iconBound.mSize + dp2px(2));
-        textBound.mStyle = ta.getInt(R.styleable.XZIconFontView_font_rightTextStyle, Typeface.NORMAL);
-        mBoundsMap.put(RIGHT, textBound);
-
-        // 下
-        textBound = new TextBound();
-        textBound.mText = ta.getText(R.styleable.XZIconFontView_font_bottomText);
-        textBound.mColor = ta.getColor(R.styleable.XZIconFontView_font_bottomTextColor, iconBound.mColor);
-        textBound.mSize = ta.getDimension(R.styleable.XZIconFontView_font_bottomTextSize, iconBound.mSize + dp2px(2));
-        textBound.mStyle = ta.getInt(R.styleable.XZIconFontView_font_bottomTextStyle, Typeface.NORMAL);
-        mBoundsMap.put(BOTTOM, textBound);
+        mTextBound.orientation = ta.getInt(R.styleable.XZIconFontView_text_Orientation, LEFT);
+        mTextBound.mText = ta.getText(R.styleable.XZIconFontView_font_Text);
+        mTextBound.mColor = ta.getColor(R.styleable.XZIconFontView_font_TextColor, mIconBound.mColor);
+        mTextBound.mSize = ta.getDimension(R.styleable.XZIconFontView_font_TextSize, mIconBound.mSize + dp2px(2));
+        mTextBound.mStyle = ta.getInt(R.styleable.XZIconFontView_font_TextStyle, Typeface.NORMAL);
+        mTextBound.textPadding = (int) ta.getDimension(R.styleable.XZIconFontView_textPadding, 0);
         ta.recycle();
 
-        initView(iconBound, leftPadding, topPadding, rightPadding, bottomPadding);
+        // 初始化IconView
+        rendererIconView();
 
-
-    }
-
-    private void initView(TextBound iconBound, int leftPadding, int topPadding, int rightPadding, int bottomPadding) {
-        TextBound textBound;// 设置图标的属性
-        mIconView.setText(iconBound.mText);
-        mIconView.setTextColor(iconBound.mColor);
-        mIconView.setTextSize(TypedValue.COMPLEX_UNIT_PX, iconBound.mSize);
-        mIconView.getPaint().setFakeBoldText(Typeface.BOLD == iconBound.mStyle);
-        mIconView.setPadding(leftPadding, topPadding, rightPadding, bottomPadding);
-
-        // text 相关
-        textBound = mBoundsMap.get(LEFT);
-        if (textBound.isValid()) {
-            getTextView(LEFT);
-            textBound.createAndInitText(LEFT);
-        }
-
-        textBound = mBoundsMap.get(TOP);
-        if (textBound.isValid()) {
-            textBound.createAndInitText(TOP);
-        }
-
-        textBound = mBoundsMap.get(RIGHT);
-        if (textBound.isValid()) {
-            textBound.createAndInitText(RIGHT);
-        }
-
-        textBound = mBoundsMap.get(BOTTOM);
-        if (textBound.isValid()) {
-            textBound.createAndInitText(BOTTOM);
+        // 初始化 TextView
+        if (mTextBound.isValid()) {
+            rendererTextView();
         }
     }
+
+    private void rendererIconView() {
+        mIconView.setRotation(mIconBound.orientation);
+        mIconView.setText(mIconBound.mText);
+        mIconView.setTextColor(mIconBound.mColor);
+        mIconView.setTextSize(TypedValue.COMPLEX_UNIT_PX, mIconBound.mSize);
+        mIconView.getPaint().setFakeBoldText(Typeface.BOLD == mIconBound.mStyle);
+    }
+
+
+    /**
+     * 创建并设置TextView视图
+     */
+    private void rendererTextView() {
+        // 初次创建，加入到父布局中
+        if (mTextView == null) {
+            mTextView = new TextView(getContext());
+            mTextView.setId(R.id.tv_id);
+            addView(mTextView, -2, -2);
+        }
+        LayoutParams icon_lp = new LayoutParams(-2, -2);
+        boolean showIt = !TextUtils.isEmpty(mTextBound.mText);
+        if (showIt) {
+            // 描述位置关系
+            LayoutParams text_lp = new LayoutParams(-2, -2);
+            // init LayoutParams
+            switch (mTextBound.orientation) {
+                case LEFT:
+                    text_lp.addRule(CENTER_VERTICAL, TRUE);
+                    icon_lp.addRule(CENTER_VERTICAL, TRUE);
+
+                    icon_lp.addRule(RIGHT_OF, mTextView.getId());
+                    icon_lp.leftMargin = mTextBound.textPadding;
+                    break;
+                case TOP:
+                    icon_lp.addRule(CENTER_HORIZONTAL, TRUE);
+                    text_lp.addRule(ALIGN_PARENT_TOP, TRUE);
+                    text_lp.addRule(CENTER_HORIZONTAL, TRUE);
+
+                    icon_lp.addRule(BELOW, mTextView.getId());
+                    break;
+                case RIGHT:
+                    text_lp.addRule(CENTER_VERTICAL, TRUE);
+                    icon_lp.addRule(CENTER_VERTICAL, TRUE);
+
+                    text_lp.addRule(RIGHT_OF, mIconView.getId());
+                    icon_lp.leftMargin = mTextBound.textPadding;
+
+                    break;
+                case BOTTOM:
+                    icon_lp.addRule(CENTER_HORIZONTAL, TRUE);
+                    text_lp.addRule(CENTER_HORIZONTAL, TRUE);
+
+                    text_lp.addRule(BELOW, mIconView.getId());
+                    break;
+            }
+            mTextView.setLayoutParams(text_lp);
+
+            // 设置text样式
+            mTextView.setText(mTextBound.mText);
+            mTextView.setTextColor(mTextBound.mColor);
+            mTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX, mTextBound.mSize);
+            TextPaint textPaint = mTextView.getPaint();
+            textPaint.setFakeBoldText(Typeface.BOLD == mIconBound.mStyle || Typeface.BOLD_ITALIC == mTextBound.mStyle);// 粗体
+            textPaint.setTextSkewX(Typeface.ITALIC == mIconBound.mStyle || Typeface.BOLD_ITALIC == mTextBound.mStyle ? -0.5f : 0);//float类型参数，负数表示右斜，整数左斜
+        }
+        mIconView.setLayoutParams(icon_lp);
+        mTextView.setVisibility(showIt ? VISIBLE : GONE);
+    }
+
 
     /**
      * 设置信息
@@ -160,10 +169,50 @@ public class XZIconFontView extends RelativeLayout {
      * @param iconCode icon信息
      */
     public void setIcon(CharSequence iconCode) {
-        //  文字渲染
-        mIconView.setText(iconCode);
+        mIconBound.mText = iconCode;
+        rendererIconView();
     }
 
+
+    /**
+     * 设置icon大小
+     *
+     * @param size
+     */
+    public void setIconSize(int size) {
+        mIconBound.mSize = size;
+        rendererIconView();
+    }
+
+    /**
+     * 设置icon颜色
+     *
+     * @param iconColor
+     */
+    public void setIconColor(@ColorInt int iconColor) {
+        mIconBound.mColor = iconColor;
+        rendererIconView();
+    }
+
+    /**
+     * 设置icon是否粗体
+     *
+     * @param isBold
+     */
+    public void setIconBold(boolean isBold) {
+        mIconBound.mStyle = isBold ? Typeface.BOLD : Typeface.NORMAL;
+        rendererIconView();
+    }
+
+    /**
+     * 设置icon的偏转角度
+     *
+     * @param rotation
+     */
+    public void setIconRotation(int rotation) {
+        mIconBound.orientation = rotation;
+        rendererIconView();
+    }
 
     /**
      * 动态设置是否展示icon
@@ -174,22 +223,28 @@ public class XZIconFontView extends RelativeLayout {
         mIconView.setVisibility(visible ? VISIBLE : GONE);
     }
 
+// ----------------------- text -----------------------------------------
+
     /**
-     * @param witchOne {@link #LEFT}, {@link #TOP}, {@link #RIGHT}, {@link #BOTTOM}
+     * 获取text样式数据
+     *
      * @return
      */
-    public TextView getTextView(@IntRange(from = LEFT, to = BOTTOM) int witchOne) {
-        TextBound textBound = mBoundsMap.get(witchOne);
-        if (textBound == null) {
-            textBound = new TextBound();
-            textBound.mColor = mIconView.getTextColors().getDefaultColor();
-            textBound.mSize = mIconView.getTextSize();
-            mBoundsMap.put(witchOne, textBound);
+    public TextBound getTextBound() {
+        return mTextBound.clone();
+    }
+
+    /**
+     * 设置icon附属文字信息
+     *
+     * @param textBound
+     * @return
+     */
+    public void setTextThem(TextBound textBound) {
+        if (!mTextBound.equals(textBound)) {
+            mTextBound = textBound;
+            rendererTextView();
         }
-        if (textBound.view == null) {
-            textBound.createAndInitText(witchOne);
-        }
-        return textBound.view;
     }
 
     /**
@@ -206,30 +261,6 @@ public class XZIconFontView extends RelativeLayout {
         }
     }
 
-//    /**
-//     * 重定义文字大小、颜色、样式
-//     *
-//     * @param bound 文本
-//     * @return
-//     */
-//    private SpannableStringBuilder getSpanStr(TextBound bound) {
-//        SpannableStringBuilder spannableString = new SpannableStringBuilder();
-//        // 文字
-//        if (bound != null && !TextUtils.isEmpty(bound.mText)) {
-//            spannableString.append(bound.mText);
-//            // 色值
-//            ForegroundColorSpan colorSpan = new ForegroundColorSpan(bound.mColor);
-//            spannableString.setSpan(colorSpan, 0, bound.mText.length(), Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
-//            // 字号设置
-//            AbsoluteSizeSpan absoluteSizeSpan = new AbsoluteSizeSpan((int) bound.mSize);
-//            spannableString.setSpan(absoluteSizeSpan, 0, bound.mText.length(), Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
-//            // 字体设置
-//            StyleSpan styleSpan = new StyleSpan(bound.mStyle);
-//            spannableString.setSpan(styleSpan, 0, bound.mText.length(), Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
-//        }
-//        return spannableString;
-//    }
-
 
     int dp2px(int dp) {
         final float scale = getResources().getDisplayMetrics().density;
@@ -237,51 +268,87 @@ public class XZIconFontView extends RelativeLayout {
     }
 
 
-    public class TextBound {
+    public class TextBound implements Cloneable {
 
-        private CharSequence mText;
         private float mSize = dp2px(15);
-        private @ColorInt int mColor = Color.BLACK;
+        // 样式相关信息
+        private CharSequence mText;
+        @ColorInt
+        private int mColor = Color.BLACK;
         private int mStyle = Typeface.NORMAL;
-        private TextView view;
+
+        // 位置相关信息
+        private int orientation = LEFT;
+        private int textPadding;
+
+
+        public void setOrientation(int orientation) {
+            this.orientation = orientation;
+        }
+
+
+        public void setText(CharSequence text) {
+            mText = text;
+        }
+
+
+        public void setSize(float size) {
+            mSize = size;
+        }
+
+        public void setColor(int color) {
+            mColor = color;
+        }
+
+        public void setStyle(int style) {
+            mStyle = style;
+        }
+
+        public void setTextPadding(int textPadding) {
+            this.textPadding = textPadding;
+        }
 
         private boolean isValid() {
             return !TextUtils.isEmpty(mText);
         }
 
-        /**
-         * 创建并设置TextView视图
-         * @param witchOne {@link #LEFT}, {@link #TOP}, {@link #RIGHT}, {@link #BOTTOM}
-         */
-        private void createAndInitText(int witchOne) {
-            // createView
-            view = new TextView(getContext());
-            addView(view, -2, -2);
-            // init LayoutParams
-            RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) view.getLayoutParams();
-            switch (witchOne) {
-                case LEFT:
-                    lp.addRule(RelativeLayout.LEFT_OF, mIconView.getId());
-                    break;
-                case TOP:
-                    lp.addRule(RelativeLayout.ABOVE, mIconView.getId());
-                    break;
-                case RIGHT:
-                    lp.addRule(RelativeLayout.RIGHT_OF, mIconView.getId());
-                    break;
-                case BOTTOM:
-                    lp.addRule(RelativeLayout.BELOW, mIconView.getId());
-                    break;
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            TextBound textBound = (TextBound) o;
+
+            if (orientation != textBound.orientation) return false;
+            if (Float.compare(textBound.mSize, mSize) != 0) return false;
+            if (mColor != textBound.mColor) return false;
+            if (mStyle != textBound.mStyle) return false;
+            if (textPadding != textBound.textPadding) return false;
+            return mText.equals(textBound.mText);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = orientation;
+            result = 31 * result + mText.hashCode();
+            result = 31 * result + (mSize != +0.0f ? Float.floatToIntBits(mSize) : 0);
+            result = 31 * result + mColor;
+            result = 31 * result + mStyle;
+            result = 31 * result + textPadding;
+            return result;
+        }
+
+        @Override
+        protected TextBound clone() {
+            TextBound bound;
+            try {
+                bound = (TextBound) super.clone();
+                // person.arrFavor = arrFavor.clone();
+                return bound;
+            } catch (CloneNotSupportedException e) {
+                e.printStackTrace();
             }
-            lp.addRule(witchOne % 2 == 0 ? RelativeLayout.CENTER_VERTICAL : RelativeLayout.CENTER_HORIZONTAL);
-            view.setText(mText);
-            view.setTextColor(mColor);
-            view.setTextSize(TypedValue.COMPLEX_UNIT_PX, mSize);
-
-            TextPaint textPaint = view.getPaint();
-            textPaint.setFakeBoldText(Typeface.BOLD == mStyle || Typeface.BOLD_ITALIC == mStyle);// 粗体
-            textPaint.setTextSkewX(Typeface.ITALIC == mStyle || Typeface.BOLD_ITALIC == mStyle ? -0.5f : 0);//float类型参数，负数表示右斜，整数左斜
-
+            return new TextBound();
         }
     }
 }
